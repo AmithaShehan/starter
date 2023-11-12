3c34cdbfdb05cc1e519907217a2bcd43400ad347
Amitha <amithashehan943@gmail.com>
2023-11-12 19:56:40 +0530

    rev

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu-bars');
+// let navbarCustom = document.querySelector('.navbar-custom');
+
+menu.onClick = () => {
+    // menu.classList.toggle('fa-solid fa-xmark');
+    // navbarCustom.classList.toggle('activeMenu');
+    console.log('clicked');
+    
+}
